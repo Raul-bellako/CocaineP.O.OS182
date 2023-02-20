c14b54fadcc7efb666179b35943186a43c359ad9
Raul-bellako <125909103+Raul-bellako@users.noreply.github.com>
2023-02-20 15:44:58 -0600

    Creamos la clase objetos

---
+++ objetos.py
@@ -0,0 +1,10 @@
+from personaje import *
+
+#Creamos una instancia de la clase personaje heroe
+Heroe = Personaje()
+
+#Usamos los atributos
+
+print ("El persobaje se llama" + Heroe.nombre)
+print ("Pertenece a la especie" + Heroe.especie)
+print ("Y una altura de " + str(Heroe.altura))
