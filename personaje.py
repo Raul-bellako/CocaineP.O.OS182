e4cfc8ab5d44006e0770bab47e69d180dc58077a
Raul-bellako <125909103+Raul-bellako@users.noreply.github.com>
2023-02-20 15:36:59 -0600

    se creo el metodo lanzar granada y recargar arma

--- personaje.py
+++ personaje.py
@@ -3,7 +3,6 @@ class Personaje:
     especie="Humanos"
     nombre="MasterChief"
     altura=2.70
-    
     #Metodos
     
     def correr(self, status):
@@ -11,3 +10,12 @@ class Personaje:
             print ("El personaje " + self.nombre + "Esta corriendo")
         else :
             print ("El personaje " + self.nombre + "Se detuvo")
+    
+    def lanzarGranadas (self):
+        print("El personaje" + self.nombre + "lanzo granada")
+        
+    def recargarArma(self, municiones):
+        cargador=10
+        cargador = cargador + municiones
+        print ("El arma recargada tiene" + cargador + "balas")
+        
