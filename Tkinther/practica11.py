f4224d71c4b3abe568ed2dbfa8cdf4a6576814f5
Raul-bellako <125909103+Raul-bellako@users.noreply.github.com>
2023-03-01 15:01:08 -0600

    Funcionalidades, botones, mensaje

--- Tkinther/practica11.py
+++ Tkinther/practica11.py
@@ -1,4 +1,21 @@
-from tkinter import Tk, Button, Frame
+from tkinter import Tk, Frame, Button, messagebox, simpledialog as simpledialog
+
+#5 agregar funcion de mensaje
+
+def mostrarmensaje():
+    messagebox.showinfo("informacion","Te informo que")
+    messagebox.showerror("error", "perdon te falle")
+    print(messagebox.askokcancel("Pregunta", "Seguro que quieres guardar"))
+    
+    value = simpledialog.askstring("Input", "Ingrese un valor:")
+    print("El valor ingresado es:", value)
+    
+    
+#6. Funcion agregar botones
+def agregarBoton ():
+    botonverde.config(text="+", bg = "green")
+    botonNuevo = Button (seccion3, text="Boton nuevo")
+    botonNuevo.pack()
 
 #1 Generar una ventana 
 ventana = Tk ()
@@ -17,17 +34,19 @@ seccion3.pack(expand = True, fill = 'both')
 
 #Agregamos botones
 
-botonAzul = Button(seccion1, text="Soy el azul", bg = "blue")
+botonAzul = Button(seccion1, text="Soy el azul", bg = "#4298f5", command=mostrarmensaje)
 botonAzul.place(x=60,y=60, width=100, height=30)
 
-botonamarillo = Button(seccion2, text="Soy el amarillo", bg = "yellow")
+botonamarillo = Button(seccion2, text="Soy el amarillo", bg = "#e9f542")
 botonamarillo.grid(row=1, column=1)
 
-botonnegro = Button(seccion2, text= "soy el negro", bg = "black")
+botonnegro = Button(seccion2, text= "soy el negro", bg = "#f542da")
 botonnegro.grid(row=0,column=0)
 
-botonverde = Button(seccion3, text= "soy el verde", bg = "green")
+botonverde = Button(seccion3, text= "soy el verde", bg = "#42f551", command=agregarBoton)
 botonverde.pack()
 
+
+#4 
 #Mentono Main para la ejecucion del ventama
 ventana.mainloop()
